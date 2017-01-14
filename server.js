8f0f7821833a1f92eb0140df508cdd13154e8f08
cfgswal <cfgswal@gmail.com>
2017-01-14 14:54:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,34 +21,7 @@ app.use(express.static(__dirname + "/public"));
     Devuelve un objeto JSON con el formato:
     [{"text":"Faltan 59 días..."}]
 */
-var request = require('request');
 
-app.get("/temp", function (req, res) {
-
-    var temperatura = 0;
-
-    request('http://api.geonames.org/findNearByWeatherJSON?lat=42.846718&lng=-2.671635&username=eduardo_gpg',
-        function (error, response, data) {
-            if (!error) {
-                var response = JSON.parse(data);
-                temperatura = response.weatherObservation.temperature;
-                console.log(response.weatherObservation);
-            } else {
-                temperatura = 15; //temperatura por defecto
-            }
-
-            // Crear el objeto con la respuesta
-            var respuesta = [
-                {
-                    text: "Nos encontramos a " + temperatura + " grados de temperatura",
-                }
-            ];
-
-            // Devolver el objeto en formato JSON
-            res.json(respuesta);
-
-        });
-   });
 app.get("/plazo/:fecha", function (req, res) {
 
     // Obtener la fecha que llega en la URL
