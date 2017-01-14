4b4f491e8b838db39f512a206a54bb45cbede808
cfgswal <cfgswal@gmail.com>
2017-01-14 15:00:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Importamos el framework Express y lo inicializamos
 var express = require("express");
 var app = express();
+var request = require('request');
 
 // El contenido de la carpeta public se muestra en la raíz del servidor
 app.use(express.static(__dirname + "/public"));
@@ -23,7 +24,7 @@ app.use(express.static(__dirname + "/public"));
     [{"text":"Faltan 59 días..."}]
 */
 
-var request = require('request');
+
 
 app.get("/temp", function (req, res) {
 
@@ -53,31 +54,6 @@ app.get("/temp", function (req, res) {
 
 });
 
-app.get("/plazo/:fecha", function (req, res) {
-
-    // Obtener la fecha que llega en la URL
-    var fecha_entrega = new Date(req.params.fecha);
-    var ahora = new Date();
-
-    // Algo no ha ido bien
-    if (!fecha_entrega) {
-        return res.status(400).send();
-    }
-
-    // http://stackoverflow.com/questions/2627473/how-to-calculate-the-number-of-days-between-two-dates-using-javascript
-    var oneDay = 24 * 60 * 60 * 1000;
-    var dias = Math.round(Math.abs((ahora.getTime() - fecha_entrega.getTime()) / (oneDay)));
-
-    // Crear el objeto con la respuesta
-    var respuesta = [
-        {
-            text: "Faltan " + dias + " días...",
-        }
-    ];
-
-    // Devolver el objeto en formato JSON
-    res.json(respuesta);
-});
 
 // Arrancar el servidor y dejarlo a la espera
 app.listen(PORT, function () {
