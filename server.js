6a7cfda834ddabd1b12373c64e8b762fa9740086
cfgswal <cfgswal@gmail.com>
2017-01-17 15:14:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,74 +14,6 @@ var app = express();
 // El contenido de la carpeta public se muestra en la raíz del servidor
 app.use(express.static(__dirname + "/public"));
 
-
-/*
-<<<<<<< HEAD
- GET /plazo/:fecha
-=======
-    GET /plazo/:fecha
-    Ruta que calcula el número de días que faltan para una fecha
-    Si el servidor se está ejecutando localmente:
-    http://localhost:3000/plazo/2017-01-31T23:59:59.000Z
-    Devuelve un objeto JSON con el formato:
-    [{"text":"Faltan 59 días..."}]
-*/
-
-var request = require('request');
-
-app.get("/temp", function (req, res) {
-
-    var temperatura = 0;
-
-    request('http://api.geonames.org/findNearByWeatherJSON?lat=42.846718&lng=-2.671635&username=eduardo_gpg',
-        function (error, response, data) {
-            if (!error) {
-                var response = JSON.parse(data);
-                temperatura = response.weatherObservation.temperature;
-                console.log(response.weatherObservation);
-            } else {
-                temperatura = 15; //temperatura por defecto
-            }
->>>>>>> origin/master
-
- Ruta que calcula el número de días que faltan para una fecha
-
- Si el servidor se está ejecutando localmente:
-
- http://localhost:3000/plazo/2017-01-31T23:59:59.000Z
-
- Devuelve un objeto JSON con el formato:
-
-<<<<<<< HEAD
- [{"text":"Faltan 59 días..."}]
- */
-app.get("/plazo/:fecha", function (req, res) {
-
-    // Obtener la fecha que llega en la URL
-    var fecha_entrega = new Date(req.params.fecha);
-    var ahora = new Date();
-
-    // Algo no ha ido bien
-    if (!fecha_entrega) {
-        return res.status(400).send();
-    }
-
-    // http://stackoverflow.com/questions/2627473/how-to-calculate-the-number-of-days-between-two-dates-using-javascript
-    var oneDay = 24 * 60 * 60 * 1000;
-    var dias = Math.round(Math.abs((ahora.getTime() - fecha_entrega.getTime()) / (oneDay)));
-
-    // Crear el objeto con la respuesta
-    var respuesta = [
-        {
-            text: "Faltan " + dias + " días...",
-        }
-    ];
-
-    // Devolver el objeto en formato JSON
-    res.json(respuesta);
-});
-
-
 var request = require('request');
 
 app.get("/temp", function (req, res) {
@@ -112,9 +44,6 @@ app.get("/temp", function (req, res) {
 
 });
 
-
-=======
->>>>>>> origin/master
 // Arrancar el servidor y dejarlo a la espera
 app.listen(PORT, function () {
     console.log("Servidor Express escuchando en el puerto " + PORT + "...");
